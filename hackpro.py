5a83a3d47d696556e6e7757abcf87a159adbd790
James404 <68128416+James404-cyber@users.noreply.github.com>
2021-11-01 07:50:03 +0300

    Add files via upload

---
+++ hackpro.py
@@ -0,0 +1,14 @@
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from lib import subscribe
+    subscribe()
+elif bit == '32bit':
+    from lib import subscribe
+    subscribe()
