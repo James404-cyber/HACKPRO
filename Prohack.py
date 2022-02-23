c1d1cd6abd2893bfc8e3983e0acac3c09273b13a
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-02-23 09:30:32 +0300

    Add files via upload

---
+++ Prohack.py
@@ -0,0 +1,14 @@
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Prohack import maping
+    maping()
+elif bit == '32bit':
+    from Prohack import maping
+    maping()
