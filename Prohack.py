fd4f7314b246c8f092c3e6c105123e8a611dce9c
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-02-26 18:24:37 +0300

    Update Prohack.py

--- Prohack.py
+++ Prohack.py
@@ -1,6 +1,14 @@
-#coding=utf-8
-import os,sys,subprocess
-device_arch=subprocess.check_output('uname -m',shell=True)
-if 'aarch64' in str(device_arch):
-    os.system('chmod 777 Prohack && ./Prohack')
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
 
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Prohack import maping
+    maping()
+elif bit == '32bit':
+    from Prohack import maping
+    maping()
