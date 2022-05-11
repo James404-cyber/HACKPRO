46f1b5c0bba49c63682aede98fc02cf2f7354f9f
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-05-11 19:18:00 +0300

    Update Prohack.py

--- Prohack.py
+++ Prohack.py
@@ -1,5 +1,9 @@
 import platform
-
+import os
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('touch .prox.txt')
+except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("Prohack")._site_ope_()
