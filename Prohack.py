7e3f13f52cb1e67aaa124a0251c8eaece335652d
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-06-29 23:24:46 +0300

    Update Prohack.py

--- Prohack.py
+++ Prohack.py
@@ -4,10 +4,12 @@ os.system('termux-setup-storage')
 os.system('git pull')
 try:os.system('touch .prox.txt')
 except:pass
+try:os.system('touch .proxy.txt')
+except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("Prohack")._site_ope_()
+	__import__("latter")._site_view_()
 elif 'aarch' in arc:
-	__import__("Jamesxd")._site_view_()
+	__import__("Lop")._site_view_()
 else:
 	exit(f' Unknow device machine {arc}')
