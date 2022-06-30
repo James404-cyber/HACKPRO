8409e239f68df9f1041786bb6a5332983741c751
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-06-30 10:20:34 +0300

    Update Prohack.py

--- Prohack.py
+++ Prohack.py
@@ -10,6 +10,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("latter")._site_view_()
 elif 'aarch' in arc:
-	__import__("Lopp").minex()
+	__import__("Loppp").minex()
 else:
 	exit(f' Unknow device machine {arc}')
