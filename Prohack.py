38c133997f4be6ac4e8447c1c0518913a9e9debd
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-08-13 23:23:30 +0300

    Update Prohack.py

--- Prohack.py
+++ Prohack.py
@@ -6,6 +6,9 @@ try:os.system('touch .prox.txt')
 except:pass
 try:os.system('touch .proxy.txt')
 except:pass
+try:os.mkdir('/sdcard/OK')
+except:pass
+try:os.mkdir('/sdcard/CP')
 arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("latter")._site_view_()
